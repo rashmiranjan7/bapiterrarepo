262fb06173958257b9a0b18ffadd12ac7a83fdeb
Rashmi ranjan panigrahy <rashmiranjanpanigrahy07@gmail.com>
2025-09-01 13:14:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-region = "ap-south-2"
+region = "ap-south-1"
 }
 
 resource "aws_instance" "three" {
   count         = 1
-  ami           = "ami-031eac8ecb2726fe0"
+  ami           = "ami-0c4a668b99e68bbde"
   instance_type = "t2.micro"
 
   tags = {
