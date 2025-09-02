50f34c18f5f509ee57c2a45eabae3be69dc7122b
Rashmi ranjan panigrahy <rashmiranjanpanigrahy07@gmail.com>
2025-09-02 12:25:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ region = "ap-south-1"
 
 resource "aws_instance" "three" {
   count         = 1
-  ami           = "ami-0c4a668b99e68bbde"
+  ami           = "ami-0861f4e788f5069dd"
   instance_type = "t2.micro"
 
   tags = {
