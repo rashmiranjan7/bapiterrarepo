82ac1e58698f91b444624ca162eaef5cb776807d
Rashmi ranjan panigrahy <rashmiranjanpanigrahy07@gmail.com>
2025-09-02 16:38:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,12 @@
 provider "aws" {
-region = "ap-south-1"
+  region = "ap-south-1"
 }
 
-resource "aws_instance" "three" {
-  count         = 1
-  ami           = "ami-0861f4e788f5069dd"
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-0861f4e788f5069dd" # Ubuntu 22.04 in ap-south-1
   instance_type = "t2.micro"
 
   tags = {
-    Name = "test-server"
+    Name = "my-test-instance"
   }
 }
